d8dccaec70d079039879c3780edefd64b4473034
BhavikKatyal <40070017+BhavikKatyal@users.noreply.github.com>
2019-02-01 16:11:20 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,6 +16,14 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from mycroft import MycroftSkill, intent_file_handler
+import requests
+import json
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+__author__ = 'eward'
+
+LOGGER = getLogger(__name__)
 
 
 class Skill(MycroftSkill):
@@ -27,7 +35,28 @@ class Skill(MycroftSkill):
         try:
             number = message.data.get("number")
             response = {'number': message.data.get("number")}
-            self.speak_dialog("count_start", data=response)
+            url = "https://ip0rzvwy82.execute-api.us-east-1.amazonaws.com/Test/mycroft-skill-emp-details"
+
+            key="{\n\t\"inputparams\":"
+            value="hridul gupta"
+            key1 = "\""+value+"\"\n}"
+            payload = key + key1
+            headers = {
+                'Content-Type': "application/json",
+                'Host': "ip0rzvwy82.execute-api.us-east-1.amazonaws.com"
+                }
+
+            response = requests.request("POST", url, data=payload, headers=headers)
+            data = json.loads(response.text)
+            data2 = json.loads(data['body'])
+            id_emp = data2['emp_id']
+            designation=data2['designation']
+            location=data2['location']
+            birth_date=data2['birth_date']
+            joining_date=data2['joining_date']
+            self.speak_dialog("{}, {}, location, {} , Birth Date , {} , Joining date, {}".format(id_emp,designation,location,birth_date,joining_date))
+            #self.speak_dialog("count_start", data=response)
+            
         except:
             self.speak_dialog("error")
 
